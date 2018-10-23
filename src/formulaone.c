1806902766ed5dc2bbeddb646c46c9360e9c8377
Jonathan Sands <jonathan.sands@epita.fr>
2018-10-23 12:30:43 +0200

    euh jsp ce que je fais

--- src/formulaone.c
+++ src/formulaone.c
@@ -1,4 +1,8 @@
 #include "control.h"
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
 
 enum move update(struct car *car)
 {
@@ -6,4 +10,16 @@ enum move update(struct car *car)
     return DO_NOTHING;
 }
 
+/*je sais pas ce que je branle mdr*/
 
+init_mat(struct map *map)
+{
+    struct vector2 *start = malloc(sizeof struct vecotr2);
+    start->x = map_get_start_x(map);
+    start->y = map_get_start_y(map);
+    enum floortype **height = malloc(map->height * sizeof(floortype *));
+    for(int i = 0; i < map->height; ++i)
+    {
+        height[i] = malloc(map->width * sizeof(floortype));
+    }
+}
