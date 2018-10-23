ec2fd46fedf6e45626aed5c1af45f76a5dccab2b
Jonathan Sands <jonathan.sands@epita.fr>
2018-10-23 13:54:04 +0200

    bon j ai cours

--- src/pathfinding.c
+++ src/pathfinding.c
@@ -6,11 +6,13 @@
 
 /*je sais pas ce que je branle mdr*/
 
-init_mat(struct map *map)
+struct vector *find_path(struct map *map)
 {
     struct vector2 *start = malloc(sizeof struct vecotr2);
     start->x = map_get_start_x(map);
     start->y = map_get_start_y(map);
+    struct vector *parent = vector_init(map->width * map->height);
+
     //enum floortype **height = malloc(map->height * sizeof(floortype *));
     //for(int i = 0; i < map->height; ++i)
     //{
