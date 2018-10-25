ee5d5d2f5fde63706ed0b58843a543d7ae840b21
névyr2 <clement.davin@epita?fr>
2018-10-26 00:03:32 +0200

    turn right and left

--- src/formulaone.c
+++ src/formulaone.c
@@ -4,26 +4,88 @@
 #include <math.h>
 #include <stdio.h>
 
-struct car *car_angle(struct car *car)
+
+struct vector2 *get_arrival(struct car *car)
 {
+    struct vector2 *arrival = vector2_new();
     for( int i = 0; i < car->map->width; i++)
     {
 	for (int j = 0; j < car->map->height; j++)
  	{
 	    if ( map_get_floor(car->map,i,j) == FINISH)
 	    {
-		car->direction_angle = atan((float)j - (float)map_get_start_y(car->map))/((float)i - (float)map_get_start_x(car->map)); 
-		car->direction.x = i;
-		car->direction.y = j;
+		arrival->x = i;
+		arrival->y = j;
 	    }
 	}
+    } 
+    return arrival;
+}
+
+struct vector2 *get_angle(struct car *car)
+{
+    struct vector2 *arrival = get_arrival(car);
+    struct vector2 *triangle_rec = vector2_new();
+    triangle_rec->x = car->position.x;
+    triangle_rec->y = arrival->y;
+
+    float adjacent = sqrt(pow((triangle_rec->x - car->position.x),2) + pow((triangle_rec->y - car->position.y),2)); 
+    float oppose = sqrt(pow((triangle_rec->x - arrival->x),2) + pow((triangle_rec->y - arrival->y),2));
+    float hypotenus = sqrt(pow((arrival->x - car->position.x),2) + pow((arrival->y - car->position.y),2));
+
+    float cos = adjacent/hypotenus;
+    float sin = oppose/hypotenus;
+
+    struct vector2 *angle = vector2_new();
+    angle->x = sin;
+    angle->y = -cos;
+    return angle;
+}
+
+enum move action (struct car *car)
+{
+    struct vector2 *angle = get_angle(car);
+    if (car->speed.x > 0.25f || car->speed.y > 0.25f)
+	return BRAKE;
+    if (car->direction.x > (angle->x -0.02f) && car->direction.x < (angle->x + 0.02f) 
+	    && car->direction.y > (angle->y - 0.02f) && car->direction.y < (angle->y + 0.02f))
+	return ACCELERATE;
+    if (angle->x > 0 && angle->y > 0)
+    {
+	if (car->direction.x < angle->x && car->direction.y > angle->y)
+	    return BRAKE_AND_TURN_RIGHT;
+	else
+	    return BRAKE_AND_TURN_LEFT;
     }
-    
-    return car;
+    if (angle->x > 0 && angle->y < 0)
+    {
+	if (car->direction.x > angle->x && car->direction.y > angle->y)
+	    return BRAKE_AND_TURN_RIGHT;
+	else
+	    return BRAKE_AND_TURN_LEFT;
+    }
+    if (angle->x < 0 && angle->y < 0)
+    {
+	if (car->direction.x > angle->x && car->direction.y < angle->y)
+	    return BRAKE_AND_TURN_RIGHT;
+	else
+	    return BRAKE_AND_TURN_LEFT;
+    }
+    if (angle->x < 0 && angle->y > 0)
+    {
+	if (car->direction.x < angle->x && car->direction.y < angle->y)
+	    return BRAKE_AND_TURN_RIGHT;
+	else
+	    return BRAKE_AND_TURN_LEFT;
+    }
+    return TURN_RIGHT;
 }
 enum move update(struct car *car)
 {
-    car = car;	
-    car = car_angle(car);
-    return ACCELERATE;
+    car = car;
+    struct vector2 *angle = get_angle(car);	
+    printf("angle x = %f || car x = %f \n", angle->x, car->direction.x);
+    printf("angle y = %f || car y = %f\n", angle->y, car->direction.y);
+    printf("speed x = %f || speed y = %f\n", car->speed.x, car->speed.y);
+    return action(car);
 }
