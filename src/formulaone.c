1388343b2b7f14ecf8f9b6f679057592f5524282
névyr2 <clement.davin@epita?fr>
2018-10-25 19:20:04 +0200

    advance on the car angle

--- src/formulaone.c
+++ src/formulaone.c
@@ -2,14 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <stdio.h>
 
-
+struct car *car_angle(struct car *car)
+{
+    for( int i = 0; i < car->map->width; i++)
+    {
+	for (int j = 0; j < car->map->height; j++)
+ 	{
+	    if ( map_get_floor(car->map,i,j) == FINISH)
+	    {
+		car->direction_angle = atan((float)j - (float)map_get_start_y(car->map))/((float)i - (float)map_get_start_x(car->map)); 
+		car->direction.x = i;
+		car->direction.y = j;
+	    }
+	}
+    }
+    
+    return car;
+}
 enum move update(struct car *car)
 {
-    car = car;
+    car = car;	
+    car = car_angle(car);
     return ACCELERATE;
 }
-
-/*je sais pas ce que je branle mdr*/
-
-
