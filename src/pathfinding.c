dd859c12763eb4207c04822e389cd82075b7f4b4
Jonathan Sands <misterjonathansands@gmail.com>
2018-11-08 18:46:23 +0100

    a bit cleaner, still too long line_of_sight

--- src/pathfinding.c
+++ src/pathfinding.c
@@ -13,10 +13,9 @@
 #define KMAG  "\x1B[35m"
 #define KCYN  "\x1B[36m"
 #define KWHT  "\x1B[37m"
-#define SCALE 3
-#define PORTION 2 
+#define SCALE 5
+#define PORTION 3 
 
-/*je sais pas ce que je branle mdr*/
 static int map_width = 0;
 static int map_height = 0;
 static int row_static, col_static; //postion of the point being explored
@@ -25,7 +24,6 @@ static int goal = 0; //will be set to one when path is found
 static struct vector2 *finish = NULL; //first end point found
 static int **closed = NULL;
 static enum floortype **my_map = NULL;
-//static struct vector **list_p = NULL;
 
 struct point
 {
@@ -41,48 +39,30 @@ struct point
 
 static struct point **mat_point = NULL;
 
-
-
-//check if vector2 is inside map
-/*static int in_limit_vec(struct vector2 *v)
-{
-    //printf("entering in_limit_vec\n");
-    if(!v)
-        return 0;
-    return ((v->x >= 0 && v->x < (float)map_width) && \
-            (v->y >= 0 && v->y < (float)map_height));
-}
-*/
-
 //check if coords are inside map
 static int in_limit(int y, int x)
 {
-    //printf("entering in_limit\n");
     return (y >= 0 && y < map_height && x >= 0 && x < map_width);
 }
 
 //get floortype of vector2
 static enum floortype floor_vect(struct vector2 v)
 {
-    //printf("entering floor_vect\n");
     return my_map[(int)v.y][(int)v.x];
 }
 
 // return true if vector2 is a BLOCK floortype
 static int blocked_vec(struct vector2 v)
 {
-    //printf("entering blocked_vec\n");
     enum floortype floor = floor_vect(v);
-    //printf("exiting floor_vect\n");
-    return (floor == BLOCK /*|| floor == GRASS*/);
+    return (floor == BLOCK);
 }
 
 // return true if coords is BLOCK floortype
 static int blocked(int row, int col)
 {
-    //printf("entering blocked\n");
     enum floortype floor = my_map[row][col];
-    return(floor == BLOCK /*|| floor == GRASS*/);
+    return(floor == BLOCK);
 }
 
 static int squeezed(int old_row, int old_col, int row, int col)
@@ -96,14 +76,12 @@ static int squeezed(int old_row, int old_col, int row, int col)
 // return true if vector2 is FINISH BLOCK
 static int arrived_vec(struct vector2 v)
 {
-    //printf("entering arrived_vec\n");
     return (floor_vect(v) == FINISH);
 }
 
 // return true if coords is FINISH BLOCK
 static int arrived(int row, int col)
 {
-    //printf("entering arrived\n");
     return (my_map[row][col] == FINISH);
 }
 
@@ -112,7 +90,6 @@ static void find_finish()
 {
     if(finish)
         return;
-    //printf("entering find_finish\n");
     for(int i = 0; i < map_height; ++i)
     {
         for(int j = 0; j < map_width; ++j)
@@ -126,42 +103,35 @@ static void find_finish()
             }
          }
     }
-    //printf("exiting find_finish\n");
 }
 
 //fill matrice of 0
 static void zero_fill(int **mat, int height, int width)
 {
-    //printf("entering zero_fill\n");
     for(int i = 0; i < height; ++i)
     {
         for(int j = 0; j < width; ++j)
             mat[i][j] = 0;
     }
-    //printf("exiting zero_fill\n");
 }
 
 //point is initialized to parent coords = its coords f=g=h=0
 static void init_point(struct point *p, float y, float x)
 {
-    //printf("entering init_point\n");
     p->parent_x = x;
     p->parent_y = y;
     p->h = 0.0;
     p->g = 0.0;
     p->f = 0.0;
-    //printf("exiting init_point\n");
 }
 
 //initialize point matrice to parent coords = self and h=g=f=max value
 static void init_mat_point(int height, int width)
 {
-    //printf("entering init_mat_point\n");
     for(int i = 0; i < height; ++i)
     {
         for(int j = 0; j < width; ++j)
         {
-            //printf("i = %d, j = %d\n", i, j);
             mat_point[i][j].parent_x = -1;
             mat_point[i][j].parent_y = -1;
             mat_point[i][j].h = -1;
@@ -169,32 +139,28 @@ static void init_mat_point(int height, int width)
             mat_point[i][j].f = -1;
         }
     }
-    //printf("exiting init_mat_point\n");
 }
 
-int line_of_sight(float parent_y, float parent_x, float y, float x)
+void init_los(int *dy, int *sy, int *dx, int *sx)
 {
-    float old_x = parent_x;
-    float old_y = parent_y;
-    printf("entering line_of_sight\n");
-    float sy, sx;
-    float dy = y - parent_y;
-    float dx = x - parent_x;
-    float f = 0;
-    if (dy < 0)
+    if (*dy < 0)
     {
-        dy *= -1;
-        sy = -1;
+        *dy *= -1;
+        *sy = -1;
     }
-    else
-        sy = 1;
-    if (dx < 0)
+    if (*dx < 0)
     {
-        dx *= -1;
-        sx = -1;
+        *dx *= -1;
+        *sx = -1;
     }
-    else
-        sx = 1;
+}
+
+int line_of_sight(int parent_y, int parent_x, int y, int x)
+{
+    int sy = 1, sx = 1, f = 0;
+    int dy = y - parent_y;
+    int dx = x - parent_x;
+    init_los(&dy, &sy, &dx, &sx);
     if (dx >= dy)
     {
         while (parent_x != x)
@@ -233,19 +199,11 @@ int line_of_sight(float parent_y, float parent_x, float y, float x)
             parent_y += sy;   
         }
     }
-    printf("yay there is a line of sight between %.1f %.1f and %.1f %.1f\n", old_y, old_x, y, x);
     return 1;
 }
-/*
-struct point parent(struct point p)
-{
-    return (mat_point[p.parent_y][p.parent_x]);
-}
-*/
 //makes and returns vector2 with corresponding coordinates
 static struct vector2 make_vec2(float y, float x)
 {
-    //printf("entering lake_vec2\n");
     struct vector2 v = {.x = x, .y = y};
     return v;
 }
@@ -253,25 +211,20 @@ static struct vector2 make_vec2(float y, float x)
 //calculates value of point.h at corresponding coords
 static double get_c(int row, int col, int finish_x, int finish_y)
 {
-    //printf("entering get_h\n");
     return ((double)sqrt((row - finish_y)*(row - finish_y) + \
                 (col - finish_x)*(col - finish_x)));
 }
 
 void ComputeCost(int old_y, int old_x, int y, int x)
 {
-    printf("entering ComputeCost\n");
     double new_g;
     struct point par = mat_point[old_y][old_x];
-    printf("old_y %.0f old_x %.0f\n", par.parent_y, par.parent_x);
     struct point gp = mat_point[(int)par.parent_y][(int)par.parent_x];
     if (line_of_sight(par.parent_y, par.parent_x, y, x))
     {
-        printf("entering first if\n");
         new_g = (gp.g + get_c((int)par.parent_y, (int)par.parent_x, y, x));
         if(mat_point[y][x].g < 0 || new_g < mat_point[y][x].g)
         {
-            printf("entering second if\n");
             mat_point[y][x].parent_x = par.parent_x;
             mat_point[y][x].parent_y = par.parent_y;
             mat_point[y][x].g = new_g;
@@ -279,17 +232,14 @@ void ComputeCost(int old_y, int old_x, int y, int x)
     }
     else
     {
-        printf("entering else\n");
         new_g = mat_point[old_y][old_x].g + get_c(old_y, old_x, y, x);
         if(mat_point[y][x].g < 0 || new_g < mat_point[y][x].g)
         {
-        printf("entering if of else\n");
             mat_point[y][x].parent_x = old_x;
             mat_point[y][x].parent_y = old_y;
             mat_point[y][x].g = new_g;
         }
     }
-    printf("exiting ComputeCost\n");
 }
 
 /*Builds path vector in correct order
@@ -297,7 +247,6 @@ void ComputeCost(int old_y, int old_x, int y, int x)
   only has rounded numbers*/
 static void get_path(struct vector *path, int row, int col)
 {
-    printf("entering get_path\n");
     int row_tmp;
     if(!path)
     {
@@ -306,27 +255,23 @@ static void get_path(struct vector *path, int row, int col)
     while(!(mat_point[row][col].parent_y == row && \
                 mat_point[row][col].parent_x == col))
     {
-        path = vector_insert(path, 0, make_vec2((float)(row) / SCALE, (float)(col) / SCALE));
-        printf("exiting make_vec2\n");
+        path = vector_insert(path, 0, 
+        make_vec2((float)(row) / SCALE, (float)(col) / SCALE));
         row_tmp = mat_point[row][col].parent_y;
         col = mat_point[row][col].parent_x;
         row = row_tmp;
     }
-    //path = vector_insert(path, 0, make_vec2(row, col));
-    printf("exiting get_path\n");
 }
 /*explore point mat_point[row][col] next to mat_point[row_static][col_static]
   and finds end or modifies f, g, h values if needed*/
 static void explore(struct vector *path, int row, int col, struct vector *open)
 {
-    printf("entering explore\n");
     if(goal)
         return;
     if(in_limit(row, col) && !squeezed(row_static, col_static, row, col))
     {
         if(arrived(row, col)) //we reached end point
         {
-            printf("arrived %d %d\n", row, col);
             ComputeCost(row_static, col_static, row, col);
             get_path(path, row, col); //trace back path
             goal = 1; //YAY we arrived at the finish line
@@ -337,30 +282,23 @@ static void explore(struct vector *path, int row, int col, struct vector *open)
         {                                            //and not explored 
             ComputeCost(row_static, col_static, row, col);
             double new_h = get_c(row, col, (int)finish->y, (int)finish->x);
-            printf("exiting get_h\n");
-            printf("%d %d\n", row, col);
             double new_f = mat_point[row][col].g + new_h;
             if(mat_point[row][col].f < 0 || mat_point[row][col].f > new_f)
             {
                 struct vector2 tmp = make_vec2(row, col);
                 open = vector_remove_v2(open, tmp);
-                printf("\ngot to append in size: %lu capacity: %lu\n\n", open->size, open->capacity);
                 open = vector_append(open, tmp); //to be explored
-                vector_print(open);
-                printf("exiting make_vec2\n");
                 mat_point[row][col].h = new_h;
                 mat_point[row][col].f = new_f;
             }
 
         }
     }
-    printf("exiting explore\n");
 }
 
 //explore all 8 directions
 static void explore_all(struct vector *path, struct vector *open)
 {
-    printf("entering explore_all\n");
     explore(path, row_static - 1, col_static, open); //above
     explore(path, row_static + 1, col_static, open); //under
     explore(path, row_static, col_static - 1, open); //left
@@ -369,7 +307,6 @@ static void explore_all(struct vector *path, struct vector *open)
     explore(path, row_static - 1, col_static + 1, open); //upper-right
     explore(path, row_static + 1, col_static - 1, open); //lower-left
     explore(path, row_static + 1, col_static + 1, open); //lower-right
-    printf("exiting explore_all\n");
 }
 
 void contour(int y, int x)
@@ -412,7 +349,6 @@ void fill_my_map(struct map *map, int height, int width)
 //initializing some static variables
 static void init_static()
 {
-    printf("entering init_static\n");
     closed = malloc(map_height * (sizeof(int *)));
     mat_point = malloc(map_height * sizeof(struct point *));
     for(int i = 0; i < map_height; ++i)
@@ -423,12 +359,10 @@ static void init_static()
    
     zero_fill(closed, map_height, map_width); 
     init_mat_point(map_height, map_width);
-    printf("exiting init_static\n");
 }
 
 static void free_all(struct vector *open)
 {
-    printf("entering free_all\n");
     for(int i = 0; i < map_height; ++i)
     {
         free(closed[i]);
@@ -440,13 +374,11 @@ static void free_all(struct vector *open)
     free(mat_point);
     free(finish);
     vector_destroy(open);
-    printf("exiting free_all\n");
 }
 
 //initializes map_height and map_width
 static void get_real_map(struct map *map)
 {
-    //printf("entering get_width\n");
     if(!my_map)
     {
         map_width = map->width * SCALE;
@@ -457,41 +389,9 @@ static void get_real_map(struct map *map)
         fill_my_map(map, map_height , map_width);
 
     }
-    //printf("exiting get_width\n");
 }
-/*
-static int is_checkpoint(int y, int x, struct vector *path)
-{
-    for(size_t i = 0; i < path->size; ++i)
-    {
-        if((int)path->data[i].y == y && (int)path->data[i].x == x)
-            return 1;
-    }
-    return 0;
-}
-*/
-/*
-static char quick_char(enum floortype floor)
-{
-    //printf("entering floor_to_char\n");
-    switch (floor)
-    {
-        case ROAD:
-            return 'r';
-        case GRASS:
-            return 'g';
-        case BLOCK:
-            return 'b';
-        case FINISH:
-            return 'f';
-    }
-    return 0;
-    //printf("exiting floor_to_char\n");
-}
-*/
 static char floor_to_char(int y, int x, struct map *map)
 {
-    //printf("entering floor_to_char\n");
     switch (map_get_floor(map, x, y))
     {
         case ROAD:
@@ -504,12 +404,10 @@ static char floor_to_char(int y, int x, struct map *map)
             return 'f';
     }
     return 0;
-    //printf("exiting floor_to_char\n");
 }
 
 static char **create_matrix(struct vector *path, struct map *map)
 {
-    //printf("entering create_matrix\n");
     char **mat = malloc(map->height * sizeof(char *));
     for(int i = 0; i < map->height; ++i)
     {
@@ -522,29 +420,24 @@ static char **create_matrix(struct vector *path, struct map *map)
             mat[i][j] = floor_to_char(i, j, map);
         }
     }
-    //printf("path size = %lu\n", path->size);
     for(size_t i = 0; i < path->size; ++i)
     {
         mat[(int)path->data[i].y][(int)path->data[i].x] += ('A' - 'a');
     }
-    //printf("exiting create_matrix\n");
     return mat;
 }
 
 static void free_mat(char **mat, int height)
 {
-    //printf("entering free_mat\n");
     for(int i = 0; i < height; ++i)
     {
         free(mat[i]);
     }
     free(mat);
-    //printf("exiting free_mat\n");
 }
 
 static void print_cell(char c)
 {
-    //printf("entering print_cell\n");
     if(c < 'a')
     {
         printf("%s%c%s", KRED, c, KWHT);
@@ -564,12 +457,10 @@ static void print_cell(char c)
             printf("%s%c%s", KYEL, c, KWHT);
             break;
     }
-    //printf("exiting print_cell\n");
 }
 
 static void print_map(struct vector *path, struct map *map)
 {
-    //printf("entering print_map\n");
     char **mat = create_matrix(path, map);
     for(int i = 0; i < map->height; ++i)
     {
@@ -580,7 +471,6 @@ static void print_map(struct vector *path, struct map *map)
         printf("\n");
     }
     free_mat(mat, map->height);
-    //printf("exiting print_map\n");
 }
 
 struct vector2 get_start(struct map *map)
@@ -594,22 +484,14 @@ struct vector2 get_start(struct map *map)
 // A* path finding algorithm on map
 struct vector *find_path(struct map *map)
 {
-    printf("entering findpath\n");
     get_real_map(map); //initialize static map_width and map_height
     struct vector2 start = get_start(map);
     struct vector *path = vector_init(8);
     
     find_finish(); //set static vector2 finish variable;
-    if(!finish)
-    {
-        printf("no finish");
-    }
     //if initial conditions don't make sense
-    if(blocked_vec(start) || blocked_vec(*finish) || arrived_vec(start))
-    {
-        printf("exiting in_limit_vec false * 2, blocked true * 2, arrived_vec true\n");
+    if(!finish || blocked_vec(start) || arrived_vec(start))
         goto fail_init; //frees memory and returns NULL(path didn't change);
-    }
 
     init_static(); //initializing mat_point, closed and open static variables
 
@@ -631,24 +513,12 @@ struct vector *find_path(struct map *map)
         closed[row_static][col_static] = 1; //explored
         explore_all(path, open);
         if(goal)
-        {
             break;
-        }
     }
-    if(goal)
-        printf("goal found !!!!!!!!!!!!!!!!!!!!!!!!!!\n");
-    else
-        printf("no goal lol\n");
     print_map(path, map);
 
 fail_init: //free allocated stuff and return the path(NULL if not found)
     free_all(open);
-    if(path)
-    {
-        printf("path found\n");
-        printf("path size = %lu", path->size);
-        printf("capacity = %lu", path->capacity);
-    }
     vector_print(path);
     return path;
 }
